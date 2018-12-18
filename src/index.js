2162bc89dbf9313f8d2c12f7112477eafa4792e1
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 11:13:53 -0500

    implemented material-ui custom theme and CSSBaseline

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import theme from './theme/theme';
+
+ReactDOM.render(
+	<MuiThemeProvider theme={theme}>
+		<CssBaseline />
+		<App />
+	</MuiThemeProvider>,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
