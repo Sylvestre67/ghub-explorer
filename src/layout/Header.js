57d8b11f2b7c8eff93fade3389cdad9799eeb2d6
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 11:50:44 -0500

    added Header component

---
+++ src/layout/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const isEven = process.env.REACT_APP_IS_EVEN;
+
+const styles = theme => ({
+	grow: {
+		flexGrow: 1,
+	},
+	img: {
+		width: '50px',
+		height: 'auto',
+	},
+});
+
+export function Header(props) {
+	const { classes } = props;
+
+	return (
+		<AppBar position="fixed" elevation={0}>
+			<Toolbar>
+				{isEven ? (
+					<div>
+						<img
+							className={classes.img}
+							src={`${process.env.PUBLIC_URL}/img/even.png`}
+							alt="even"
+						/>
+					</div>
+				) : (
+					<Typography
+						variant="h6"
+						color="inherit"
+						className={classes.grow}>
+						GHub Explorer
+					</Typography>
+				)}
+			</Toolbar>
+		</AppBar>
+	);
+}
+
+Header.propTypes = {
+	classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
---
+++ src/layout/Header.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { Header } from './Header';
+let component;
+
+const props = {
+	classes: {
+		grow: '',
+	},
+};
+
+describe('Header', () => {
+	beforeAll(() => {
+		component = shallow(<Header {...props} />);
+	});
+
+	it('should render correctly', () => {
+		expect(component).toMatchSnapshot();
+	});
+});
