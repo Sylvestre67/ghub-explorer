757e1f0a02e79ed12bc00cd9cd37bcdd46110db9
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 11:14:21 -0500

    updated App sample message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
+import Typography from '@material-ui/core/Typography';
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <p>Hello World !</p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<Typography>Hello World !</Typography>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
