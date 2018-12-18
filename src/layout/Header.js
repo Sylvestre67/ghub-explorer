c94feb836e2d4f1e178db8ffad84cc7524a026b8
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 14:37:27 -0500

    Header to handle XXL screen

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core';
 
+import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -17,6 +18,10 @@ const styles = theme => ({
 		width: '50px',
 		height: 'auto',
 	},
+	bar: {
+		display: 'flex',
+		justifyContent: 'center',
+	},
 });
 
 export function Header(props) {
@@ -24,23 +29,29 @@ export function Header(props) {
 
 	return (
 		<AppBar position="relative" elevation={0}>
-			<Toolbar>
-				{isEven ? (
-					<div>
-						<img
-							className={classes.img}
-							src={`${process.env.PUBLIC_URL}/img/even.png`}
-							alt="even"
-						/>
-					</div>
-				) : (
-					<Typography
-						variant="h6"
-						color="inherit"
-						className={classes.grow}>
-						GHub Explorer
-					</Typography>
-				)}
+			<Toolbar className={classes.bar}>
+				<Grid container>
+					<Grid item xs={12}>
+						{isEven ? (
+							<div>
+								<img
+									className={classes.img}
+									src={`${
+										process.env.PUBLIC_URL
+									}/img/even.png`}
+									alt="even"
+								/>
+							</div>
+						) : (
+							<Typography
+								variant="h6"
+								color="inherit"
+								className={classes.grow}>
+								GHub Explorer
+							</Typography>
+						)}
+					</Grid>
+				</Grid>
 			</Toolbar>
 		</AppBar>
 	);
