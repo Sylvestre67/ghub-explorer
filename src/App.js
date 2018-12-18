065cc66629c516e1e731e641e64911b8f84e0337
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 11:50:55 -0500

    added Header to App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 import Typography from '@material-ui/core/Typography';
 
+import Header from './layout/Header';
+
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
+				<Header />
 				<Typography>Hello World !</Typography>
 			</React.Fragment>
 		);
