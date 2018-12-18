ddc112a054a13882390804dbbdb04a7dbf9dc4cc
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 12:20:23 -0500

    added footer to App + layout adjustment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
 
+import { withStyles } from '@material-ui/core';
+
 import Typography from '@material-ui/core/Typography';
 
 import Header from './layout/Header';
+import Footer from './layout/Footer';
+
+const styles = theme => ({
+	root: {
+		height: '100%',
+		display: 'flex',
+		flexFlow: 'column',
+	},
+	body: {
+		flexGrow: '1',
+		backgroundColor: 'white',
+	},
+});
 
 class App extends Component {
 	render() {
+		const { classes } = this.props;
+
 		return (
-			<React.Fragment>
+			<div className={classes.root}>
 				<Header />
-				<Typography>Hello World !</Typography>
-			</React.Fragment>
+				<div className={classes.body}>
+					<Typography>Hello World !</Typography>
+				</div>
+				<Footer />
+			</div>
 		);
 	}
 }
 
-export default App;
+export default withStyles(styles)(App);
