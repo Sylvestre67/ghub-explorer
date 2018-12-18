89bc3e95c1f9bf4205b8a444d0be9bead3a38ff5
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-18 12:44:49 -0500

    added jumbo to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core';
 
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import Header from './layout/Header';
@@ -17,7 +18,15 @@ const styles = theme => ({
 		flexGrow: '1',
 		backgroundColor: 'white',
 	},
+	jumbo: {
+		textAlign: 'center',
+		padding: `${theme.spacing.unit * 3}px 0`,
+	},
 });
+
+const site_title = `${
+	process.env.REACT_APP_WEBSITE_NAME
+} Github Repository Search`;
 
 class App extends Component {
 	render() {
@@ -27,7 +36,23 @@ class App extends Component {
 			<div className={classes.root}>
 				<Header />
 				<div className={classes.body}>
-					<Typography>Hello World !</Typography>
+					<Grid container>
+						<Grid item xs className={classes.jumbo}>
+							<Typography variant="h5">{site_title}</Typography>
+						</Grid>
+					</Grid>
+
+					<Grid container>
+						<Grid item xs>
+							<Typography>Form</Typography>
+						</Grid>
+					</Grid>
+
+					<Grid container>
+						<Grid item xs>
+							<Typography>Results</Typography>
+						</Grid>
+					</Grid>
 				</div>
 				<Footer />
 			</div>
