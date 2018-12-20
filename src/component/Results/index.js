f04e08fb6a9029536785246f05da69cb053b44b0
Sylvestre Gug <sylvestre.gug@gmail.com>
2018-12-20 10:18:44 -0500

    Results to subscribe to ResultsContext

--- src/component/Results/index.js
+++ src/component/Results/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import { ResultsContext } from '../../context/results.context';
 
 export class Results extends React.Component {
 	render() {
-		return <div>Results</div>;
+		return <div>{JSON.stringify(this.context, 4, 4)}</div>;
 	}
 }
+
+Results.contextType = ResultsContext;
 
 Results.propTypes = {};
 
